5f1c9b3381cae8faed64fcfbe71b70171f2e2899
vagrant <vagrant@vagrant.vm>
2017-01-06 21:37:16 +0000

    yay

---
+++ controllers/main.py
@@ -0,0 +1,13 @@
+from flask import *
+
+from extensions import connect_to_database
+
+main = Blueprint('main', __name__, template_folder='templates')
+
+@main.route('/')
+def main_route():
+    return render_template("index.html")
+
+@main.route('/hello')
+def main_hello():
+    return "Hello everyone!"
