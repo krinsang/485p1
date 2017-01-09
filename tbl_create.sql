22482ce3871e167ec7083ea2c000761b3a147694
krinsang <krinsang@umich.edu>
2017-01-08 22:47:23 -0500

    Update tbl_create.sql

--- tbl_create.sql
+++ tbl_create.sql
@@ -4,3 +4,17 @@ CREATE TABLE User (
   last_name varchar(20),
   password varchar(20),
   email TEXT(40));
+
+CREATE TABLE Album (
+  albumid int AUTO_INCREMENT,
+  title varchar(50),
+  created TIMESTAMP,
+  lastupdated TIMESTAMP,
+  username varchar(20));
+ 
+ CREATE TABLE Contain (
+   sequencenum int AUTO_INCREMENT,
+   albumid int,
+   picid varchar(40),
+   caption NOT_NULL varchar(255));
+   
