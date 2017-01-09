5fb399a764f3db442d50d24e183943f563902984
emcai <emcai@umich.edu>
2017-01-08 23:24:26 -0500

    Loaded the first three user's data

--- load_data.sql
+++ load_data.sql
@@ -1,1 +1,9 @@
+# User: username, firstname, lastname, password, email
+# Album: albumid, title, created, lastupdated, username
+# Contain: sequencenum, albumid, picid, caption
+# Photo: picid, format, date
 
+insert into User values 
+	('sportslover', 'Paul', 'Walker', 'paulpass93', 'sportslover@hotmail.com'),
+	('traveler', 'Rebecca', 'Travolta', 'rebeccapass15', 'rebt@explorer.org'),
+	('spacejunkie', 'Bob', 'Spacey', 'bob1pass', 'bspace@spacejunkies.net');
