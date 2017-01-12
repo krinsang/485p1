4a3f69c324099452b589a70833e7ac863571c2d6
afwilkin <afwilkin@umich.edu>
2017-01-12 10:05:43 -0500

    updated to connect to database

--- controllers/main.py
+++ controllers/main.py
@@ -11,3 +11,13 @@ def main_route():
 @main.route('/hello')
 def main_hello():
     return "Hello everyone!\n"
+    db = connect_to_database()
+    cur = db.cursor()
+    cur.execute('SELECT id, name FROM test_tbl')
+    results = cur.fetchall()
+    print(results)
+    print_str = "<table>"
+    for result in results:
+        print_str += "<tr><td>%s</td><td>%s</td><tr>" % (result['id'], result['name'])
+    print_str += "</table>"
+    return print_str
